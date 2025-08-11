a2ea8614e34e80e922818fc3209baaac310345b9
AbhishekDNair <abhishekdnair23@gmail.com>
2025-08-11 21:26:37 +0530

    Create sumof2numbers.c

---
+++ sumof2numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{   
+    int num1,num2,sum=0;
+    printf("Enter two numbers:");
+    scanf("%d %d",&num1,&num2);
+    sum = num1+num2;
+    printf("the sum is %d",sum);
+
+    return 0;
+}
