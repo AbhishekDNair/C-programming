b1e1759abb50f8c73f9fec27d9734db8be769175
AbhishekDNair <abhishekdnair23@gmail.com>
2025-08-11 21:22:47 +0530

    Create arithmeticoperations.c

---
+++ arithmeticoperations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Arithemntic Operators\n");
+    int a=10,b=5;
+    printf("\na+b = %d",a+b);
+    printf("\na-b = %d",a-b);
+    printf("\na*b = %d",a*b);
+    printf("\na/b = %d",a/b);
+    printf("\na%b = %d",a%b);
+
+    return 0;
+}
