6cd071955a1da69461f5f0c3b417e295df896157
AbhishekDNair <abhishekdnair23@gmail.com>
2025-08-16 23:34:08 +0530

    Create for_evenlimit.c

---
+++ for_evenlimit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,limit=50;
+    for(i=1;i<=limit;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\t",i);
+        }
+    }
+    
+    
+    return 0;
+}
