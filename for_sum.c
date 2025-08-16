f067826dd98ed16293ed83b927734afc2a6bd19a
AbhishekDNair <abhishekdnair23@gmail.com>
2025-08-16 23:32:14 +0530

    Create for_sum.c

---
+++ for_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,sum=0,num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        sum=sum+i;
+    }
+    printf("sum=%d",sum);
+    
+    return 0;
+}
