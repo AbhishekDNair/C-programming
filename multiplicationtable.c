3109806f3ba688d21ef8bafb04a9b320d364fe50
AbhishekDNair <abhishekdnair23@gmail.com>
2025-08-22 02:03:30 +0530

    Create multiplicationtable.c

---
+++ multiplicationtable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,result;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        result=n*i;
+        printf("%d  *  %d  = %d\n",n,i,result);
+    }
+    return 0;
+}
